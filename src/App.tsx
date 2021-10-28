270ce3e8efc4da6ad0ed7e045292be9c44fe6d58
Marcos Asensio <60777608+MKS2508@users.noreply.github.com>
2021-10-28 12:49:50 -0700

    v0.2.1
    Added metadata to downloaded songs!

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,14 @@ import {Button, CustomCard} from '@tsamantanis/react-glassmorphism'
 import '@tsamantanis/react-glassmorphism/dist/index.css'
 import vinyl from './vinyl.svg'
 import pepe from './3nRK.gif'
-
 import soundcloudsvg from './soundcloud.svg'
 import soundcloudgif1 from './soundcloudgif1.gif'
 import soundcloudgif2 from './soundcloudgif2.gif'
 import soundcloudgif3 from './soundcloudgif3.gif'
-
+// @ts-ignore
+import ID3Writer from 'browser-id3-writer'
 import snoop from './6os.gif'
-import axios from "axios";
-import cheerio from "cheerio";
+
 
 
 function App() {
@@ -27,93 +26,87 @@ function App() {
 
     const getArtwork = async (url: string) => {
         const track = await soundcloud.tracks.getV2(url)
-        console.log(track.full_duration);// a  mins
         serUrlArt(track.artwork_url);
     }
 
-    const getURL2 = () => {
-        const tag = document.querySelector('a.playbackSoundBadge__titleLink.sc-truncate') as HTMLAnchorElement;
-        console.log(tag)
-        return tag?.href ?? "No data";
-    }
+    const downloadWithMetadata = async () => {
+        console.log('OK')
+        let url = urlcontent;
+        const streamTrack = await soundcloud.util.streamLink(url) // streamable track - url
+        const streamMetadata = await soundcloud.tracks.getV2(url) // all extra title metadata
+        const streamDetails = await soundcloud.util.getTitle(url) // track title
+        const addMetadataAndDownload = (url:string, url2: string) =>{
 
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.responseType = 'arraybuffer';
+            let  arrayBuffer = xhr.response;
+            xhr.onload = ()=> {
+                if (xhr.status === 200) {
+
+                    arrayBuffer = xhr.response;
+
+                    const xhr2 = new XMLHttpRequest();
+                    xhr2.open('GET', url2, true);
+                    xhr2.responseType = 'arraybuffer';
+                    xhr2.onload = ()=> {
+                        if (xhr2.status === 200) {
+                            console.log({response: xhr.response})
+                            console.log({response2: xhr2.response})
+
+                            const writer = new ID3Writer(xhr.response);
+                            writer.setFrame('TIT2', streamDetails)
+                                .setFrame('TPE1', [streamMetadata.user.username])
+                                .setFrame('TYER', parseInt(streamMetadata.release_date))
+                                .setFrame('TCON', [streamMetadata.genre])
+                                .setFrame('WPAY', 'SOUNDCLOUD')
+                                .setFrame('APIC', {
+                                    type: 3,
+                                    data: xhr2.response,
+                                    description: 'Cover picture'
+                                });
+                            writer.addTag();
+                            const downurl = writer.getURL();
+                            chrome.downloads.download({url: downurl, filename: `${streamDetails}.mp3`})
+                            //setUrlDown()
+                        }
+                    };
+                    xhr2.send();
+                    // go next
+                } else {
+                    // handle error
+                    console.error(xhr.statusText + ' (' + xhr.status + ')');
+                }
+            };
+            xhr.onerror = ()=> {
+                // handle error
+                console.error('Network error');
+            };
+            xhr.send();
+            return arrayBuffer
+        }
+        const RawMP3InArrayBuffer = addMetadataAndDownload(streamTrack, streamMetadata.artwork_url)
+    }
 
     const [content, setContent] = useState('');
     const [urlcontent, seturlContent] = useState('und');
     const [urlart, serUrlArt] = useState('und');
 
-
     useEffect(() => {
         chrome.tabs.query({currentWindow: true, active: true}, tabs => {
             const currentTabID = tabs[0].id!;
-            console.warn(tabs)
-            //const currentTabURL = getURL(currentTabID)
             const actualTabURL = tabs[0].url;
             // @ts-ignore
             seturlContent(actualTabURL)
-
-            // @ts-ignore
-
-
         });
-
     }, []);
 
     if (urlcontent !== 'und') {
         getTitle(urlcontent);
         getArtwork(urlcontent)
-        /** axios.get(urlcontent)
-         .then((response) => {
-                let $ = cheerio.load(response.data);
-                console.log(response.data)
-                //console.log($('body'))
-                let x = $('div');
-                let y = $('div.playControls__elements');
-
-                console.log('OK')
-                console.log(x)
-                console.log(y)
-
-            }).catch(function (e) {
-            console.log(e);
-        })*/
     }
 
-    const download = async () => {
-
-
-        console.log('OK')
-        let url = urlcontent;
-        //url = content.url;
-        const streamTrack = await soundcloud.util.streamLink(url)
-        const streamDetails = await soundcloud.util.getTitle(url)
-        setContent(streamDetails);
-        chrome.downloads.download({url: streamTrack, filename: `${streamDetails}.mp3`})
-    }/*
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={urlart} alt="Artwork Cover" style={{width: '100px'}}/>
-
-                <p>
-                    <span className="spin"><svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" version="1.1" >
-            <g transform="translate(0 -1028.4)">
-                <path d="m12 1029.4c-6.0751 0-11 4.9-11 11 0 6 4.9249 11 11 11 6.075 0 11-5 11-11 0-6.1-4.925-11-11-11zm0 4c3.866 0 7 3.1 7 7 0 3.8-3.134 7-7 7s-7-3.2-7-7c0-3.9 3.134-7 7-7z" fill="#2c3e50"/>
-                <path d="m17 1031.7c-4.783-2.8-10.899-1.1-13.66 3.7-2.7617 4.7-1.1229 10.9 3.66 13.6 4.783 2.8 10.899 1.1 13.66-3.6 2.762-4.8 1.123-10.9-3.66-13.7zm-4 6.9c0.957 0.6 1.284 1.8 0.732 2.8-0.552 0.9-1.775 1.2-2.732 0.7-0.957-0.6-1.2843-1.8-0.732-2.7 0.552-1 1.775-1.3 2.732-0.8z" fill="#2c3e50"/>
-                <path d="m6.0098 1032.3c-2.2488 1.7-3.6216 4.2-3.9375 6.8l7.9647 1c0.065-0.6 0.33-1 0.782-1.4l-4.8092-6.4zm15.913 9.2-7.938-1c-0.065 0.6-0.357 1-0.808 1.4l4.809 6.4c2.248-1.7 3.621-4.2 3.937-6.8z" fill="#34495e"/>
-                <path d="m12 1036.4c-2.2091 0-4 1.8-4 4s1.7909 4 4 4c2.209 0 4-1.8 4-4s-1.791-4-4-4zm0 3c0.552 0 1 0.4 1 1 0 0.5-0.448 1-1 1s-1-0.5-1-1c0-0.6 0.448-1 1-1z" fill="#f1c40f"/>
-            </g>
-        </svg></span> Current track: <span style={{color: "aquamarine"}}>{content}</span>
-                </p>
-                <Button text={'Download 🎶'} onClick={() => {
-                    download()
-                }}/>
-            </header>
-        </div>
-    );*/
-
-    return (
-
         (urlcontent.startsWith("https://soundcloud.com/")) ? <div className="App">
             <div id="cardWrap">
                 <div className="card"></div>
@@ -130,7 +123,7 @@ function App() {
 
                     <div style={{marginLeft: "32%"}}>
                         <Button text={'Download 🎶'} onClick={() => {
-                            download()
+                            downloadWithMetadata()
                         }}/>
                     </div>
 
